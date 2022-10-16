ea7751e705cb45912aff9a12af6201d653b336ce
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2022-10-16 06:10:07 -0500

    added version

--- ms2ml/__init__.py
+++ ms2ml/__init__.py
@@ -1,7 +1,11 @@
+import importlib
+
 from ms2ml.config import Config
 from ms2ml.data import adapters, parsing
 from ms2ml.peptide import Peptide
 from ms2ml.spectrum import AnnotatedPeptideSpectrum, Spectrum
+
+__version__ = importlib.metadata.version("ms2ml")
 
 __all__ = [
     "adapters",
